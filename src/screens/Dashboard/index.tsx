d2054186eb07dfb8d0a4e0d6cee18ac089674660
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-07-12 23:13:02 -0300

    Config

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+//React
+import React from "react";
+
+//Styles
+import { Container, Title } from "./styles";
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Title>Hello </Title>
+    </Container>
+  );
+}
