3c9a2fc5963706aa4e0eba69f123513895651c2d
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-07-15 02:09:28 -0300

    Refactor Input Component

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { TextInputProps } from "react-native";
 
+//styled-components
+import { useTheme } from "styled-components";
+
 //Styles
 import { Container } from "./styles";
 
@@ -11,5 +14,7 @@ type Props = TextInputProps;
 export default function Input(props: Props) {
   const { ...rest } = props;
 
-  return <Container {...rest} />;
+  const theme = useTheme();
+
+  return <Container placeholderTextColor={theme.colors.text} {...rest} />;
 }
